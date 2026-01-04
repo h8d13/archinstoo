ff9422468d441368b9f5e816ff3b0df187b0ed44
HADEON <52324046+h8d13@users.noreply.github.com>
2026-01-04 01:48:36 +0100

    Revert "New uki approach"

--- archinstall/lib/models/bootloader.py
+++ archinstall/lib/models/bootloader.py
@@ -21,7 +21,7 @@ class Bootloader(Enum):
 
 	def has_uki_support(self) -> bool:
 		match self:
-			case Bootloader.Efistub | Bootloader.Limine | Bootloader.Systemd | Bootloader.Refind | Bootloader.Grub:
+			case Bootloader.Efistub | Bootloader.Limine | Bootloader.Systemd | Bootloader.Refind:
 				return True
 			case _:
 				return False
@@ -82,8 +82,7 @@ class BootloaderConfiguration:
 	def get_default(cls) -> BootloaderConfiguration:
 		bootloader = Bootloader.get_default()
 		removable = SysInfo.has_uefi() and bootloader.has_removable_support()
-		uki = SysInfo.has_uefi() and bootloader.has_uki_support()
-		return cls(bootloader=bootloader, uki=uki, removable=removable)
+		return cls(bootloader=bootloader, uki=False, removable=removable)
 
 	def preview(self) -> str:
 		text = f'{tr("Bootloader")}: {self.bootloader.value}'
