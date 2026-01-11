4fa05e1ebbba690ed18a5f7dec6db6b9cd36f862
h8d13 <hadean-eon-dev@proton.me>
2026-01-11 16:58:57 +0100

    Shorten

--- archinstall/scripts/list.py
+++ archinstall/scripts/list.py
@@ -1,7 +1,7 @@
 import glob
 from pathlib import Path
 
-print('The following are viable --script options:')
+print('Available options:')
 
 for script in [Path(x) for x in glob.glob(f'{Path(__file__).parent}/*.py')]:
 	if script.stem in ['__init__', 'list']:
