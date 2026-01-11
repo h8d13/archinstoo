c9987e84210a33a25c1d33123d01725e4f8524e5
h8d13 <hadean-eon-dev@proton.me>
2026-01-11 16:27:57 +0100

    Add rescue script and comments about mods/plugins

---
+++ archinstall/scripts/__init__.py
@@ -0,0 +1,7 @@
+# scripts are the plugin/mods system
+# they allow for simple organization
+# add a new script in this folder
+# then simply --script {name}
+# will look for a file with .py
+
+# can also use --script list to see all available
