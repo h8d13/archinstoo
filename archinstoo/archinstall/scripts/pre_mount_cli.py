839c212771cb0c3cc10f009ebddfabdfd10e46ed
h8d13 <hadean-eon-dev@proton.me>
2026-01-21 10:39:42 +0100

    Fix

--- archinstoo/archinstall/scripts/pre_mount_cli.py
+++ archinstoo/archinstall/scripts/pre_mount_cli.py
@@ -1,22 +1,12 @@
 """
-Pre-mount CLI installer from GitHub issue #4149.
+Pre-mount CLI installer from issue #4149.
 """
 
 import argparse
 from pathlib import Path
 
-from archinstall.lib.args import ArchConfig, ArchConfigHandler, Arguments
 from archinstall.lib.installer import Installer
 from archinstall.lib.models.device import DiskLayoutConfiguration, DiskLayoutType
-
-
-class MinimalConfigHandler(ArchConfigHandler):
-	"""A minimal config handler for pre-mount installs that doesn't parse CLI args."""
-
-	def __init__(self, mountpoint: Path) -> None:  # pylint: disable=super-init-not-called
-		# Intentionally don't call super().__init__() to avoid CLI arg parsing
-		self._args = Arguments(mountpoint=mountpoint)
-		self._config = ArchConfig()
 
 
 def main() -> None:
@@ -26,23 +16,16 @@ def main() -> None:
 	parser.add_argument('mountpoint', type=Path)
 	args = parser.parse_args()
 
-	handler = MinimalConfigHandler(args.mountpoint)
-
-	disk_config = DiskLayoutConfiguration(
-		config_type=DiskLayoutType.Pre_mount,
-		mountpoint=args.mountpoint,
+	installer = Installer(
+		args.mountpoint,
+		DiskLayoutConfiguration(DiskLayoutType.Pre_mount),
 	)
 
-	with Installer(
-		args.mountpoint,
-		disk_config,
-		handler=handler,
-	) as installer:
-		if args.timezone:
-			installer.set_timezone(args.timezone)
+	if args.timezone:
+		installer.set_timezone(args.timezone)
 
-		if args.hostname:
-			installer.set_hostname(args.hostname)
+	if args.hostname:
+		installer.set_hostname(args.hostname)
 
 
 main()
