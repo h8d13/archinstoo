8d52aaef349c37e0050c2a2dfb37223ce7ef7c19
h8d13 <hadean-eon-dev@proton.me>
2026-01-23 11:51:04 +0100

    bypass init

--- archinstoo/archinstall/__init__.py
+++ archinstoo/archinstall/__init__.py
@@ -9,7 +9,7 @@ import traceback
 from .lib import output
 from .lib.hardware import SysInfo
 from .lib.output import FormattedOutput, debug, error, info, log, logger, warn
-from .lib.pm import Pacman
+from .lib.pm.pacman import Pacman
 from .lib.translationhandler import Language, tr, translation_handler
 from .lib.tui.curses_menu import Tui
 from .lib.utils.env import Os, is_root, is_venv, ping, reload_python, running_from_host
