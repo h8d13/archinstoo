4bcfff2854be0e22b9925646f5623e96304af54e
h8d13 <hadean-eon-dev@proton.me>
2026-01-26 13:42:38 +0100

    rework --script list => show rootless => rename premount

--- archinstoo/archinstall/scripts/list.py
+++ archinstoo/archinstall/scripts/list.py
@@ -1,10 +1,13 @@
 import glob
 from pathlib import Path
 
+from archinstall.lib.args import ROOTLESS_SCRIPTS
+
 print('Available options:')
 
 for script in [Path(x) for x in glob.glob(f'{Path(__file__).parent}/*.py')]:
 	if script.stem in ['__init__', 'list']:
 		continue
 
-	print(f'    {script.stem}')
+	tag = '' if script.stem in ROOTLESS_SCRIPTS else '  [*] requires root'
+	print(f'    {script.stem}{tag}')
