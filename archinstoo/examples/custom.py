480f3640b2c9a87b805d5f024e4d7bbae2765963
h8d13 <hadean-eon-dev@proton.me>
2026-01-26 12:54:43 +0100

    better comments better handling

--- archinstoo/examples/custom.py
+++ archinstoo/examples/custom.py
@@ -2,12 +2,14 @@ from archinstall.default_profiles.desktop import DesktopProfile
 from archinstall.default_profiles.profile import Profile
 
 # example custom profile
+
 # this can extend an existing one or be full standalone
+
 # see archinstoo/examples/config-custom.json
 # in this example we extend xfce
 
 # for fully standalone you can just specify everything in this file and remove 
-# 'details' and 'custom_settings'
+# 'details' and 'custom_settings' from json
 
 class CustomDesktopProfile(DesktopProfile):
 	def __init__(self, current_selection: list[Profile] = []) -> None:
