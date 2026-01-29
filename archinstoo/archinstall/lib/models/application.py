ffece80da34eb62ac42a5e885f6a396fc808b444
h8d13 <hadean-eon-dev@proton.me>
2026-01-29 14:39:13 +0100

    auto select Minimal Profile and remove from list Remove openssh from hardcoded defaults

--- archinstoo/archinstall/lib/models/application.py
+++ archinstoo/archinstall/lib/models/application.py
@@ -41,6 +41,7 @@ class FirewallConfigSerialization(TypedDict):
 
 class Management(StrEnum):
 	GIT = 'git'
+	OPENSSH = 'openssh'
 	BASE_DEVEL = 'base-devel'
 	MAN = 'man-db'
 	PACMAN_CONTRIB = 'pacman-contrib'
