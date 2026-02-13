b02c412855403d97cfda728b0644a13584aa9a89
h8d13 <hadean-eon-dev@proton.me>
2026-02-13 16:28:11 +0100

    user services implrmetation

---
+++ archinstoo/archinstoo/lib/models/service.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from typing import Self, TypedDict
+
+
+class UserServiceSerialization(TypedDict):
+	unit: str
+	user: str
+	linger: bool
+
+
+@dataclass(frozen=True)
+class UserService:
+	unit: str
+	user: str
+	linger: bool = False
+
+	def json(self) -> UserServiceSerialization:
+		return {
+			'unit': self.unit,
+			'user': self.user,
+			'linger': self.linger,
+		}
+
+	@classmethod
+	def parse_arg(cls, arg: dict[str, object]) -> Self:
+		return cls(
+			unit=str(arg['unit']),
+			user=str(arg['user']),
+			linger=bool(arg.get('linger', False)),
+		)
