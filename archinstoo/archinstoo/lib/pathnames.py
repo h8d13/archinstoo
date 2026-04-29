39943a356368de6f0315c83abf499bd825514129
h8d13 <hadean-eon-dev@proton.me>
2026-04-29 11:40:46 +0200

    chore(log): shorten def

--- archinstoo/archinstoo/lib/pathnames.py
+++ archinstoo/archinstoo/lib/pathnames.py
@@ -6,3 +6,4 @@ from archinstoo.lib.linux_path import LPath
 ARCHISO_MOUNTPOINT: Final = Path('/run/archiso/airootfs')
 MIRRORLIST: Final = LPath('/etc/pacman.d/mirrorlist')
 PACMAN_CONF: Final = LPath('/etc/pacman.conf')
+ARTIFACTS_STORE: Final = LPath('etc/archinstoo.d')
