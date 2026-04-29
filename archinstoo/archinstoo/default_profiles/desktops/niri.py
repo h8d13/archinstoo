c4c3c5877016bf46b953010ad906fef5c80f8b1f
h8d13 <hadean-eon-dev@proton.me>
2026-04-29 15:51:22 +0200

    chore(desktops): revert niri change, remove polkit dep from hyprland

--- archinstoo/archinstoo/default_profiles/desktops/niri.py
+++ archinstoo/archinstoo/default_profiles/desktops/niri.py
@@ -1,7 +1,13 @@
 from typing import override
 
+from archinstoo.default_profiles.desktops import SeatAccess
 from archinstoo.default_profiles.wayland import WaylandProfile
 from archinstoo.lib.profile.base import ProfileType
+from archinstoo.lib.translationhandler import tr
+from archinstoo.lib.tui.curses_menu import SelectMenu
+from archinstoo.lib.tui.menu_item import MenuItem, MenuItemGroup
+from archinstoo.lib.tui.result import ResultType
+from archinstoo.lib.tui.types import Alignment, FrameProperties
 
 
 class NiriProfile(WaylandProfile):
@@ -29,3 +35,35 @@ class NiriProfile(WaylandProfile):
 			'xdg-desktop-portal-gnome',
 			'xdg-desktop-portal-gtk',
 		]
+
+	@property
+	@override
+	def services(self) -> list[str]:
+		if pref := self.custom_settings.get('seat_access', None):
+			return [pref]
+		return []
+
+	def _select_seat_access(self) -> None:
+		header = tr('Niri needs access to your seat (collection of hardware devices i.e. keyboard, mouse, etc)')
+		header += '\n' + tr('Choose an option to give Niri access to your hardware') + '\n'
+
+		items = [MenuItem(s.value, value=s) for s in SeatAccess]
+		group = MenuItemGroup(items, sort_items=True)
+
+		default = self.custom_settings.get('seat_access', None)
+		group.set_default_by_value(default)
+
+		result = SelectMenu[SeatAccess](
+			group,
+			header=header,
+			allow_skip=False,
+			frame=FrameProperties.min(tr('Seat access')),
+			alignment=Alignment.CENTER,
+		).run()
+
+		if result.type_ == ResultType.Selection:
+			self.custom_settings['seat_access'] = result.get_value().value
+
+	@override
+	def do_on_select(self) -> None:
+		self._select_seat_access()
