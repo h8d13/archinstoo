ab9f8915d1a117407facb0b2a34cbef8fc678a03
h8d13 <hadean-eon-dev@proton.me>
2026-06-18 20:12:25 +0200

    chore(distros): update to `flake` instead
    Signed-off-by: h8d13 <hadean-eon-dev@proton.me>

---
+++ flake.nix
@@ -0,0 +1,55 @@
+# flake.nix
+# Usage: nix develop
+# clone <url> cd
+# sudo nix develop --command ./RUN
+{
+	inputs = {
+		nixpkgs.url     = "github:NixOS/nixpkgs/nixos-unstable";
+		flake-utils.url = "github:numtide/flake-utils";
+	};
+
+	outputs = { self, nixpkgs, flake-utils, ... }:
+		flake-utils.lib.eachDefaultSystem (system: let
+			overlays = [ ];
+			pkgs = import nixpkgs {
+				inherit system overlays;
+			};
+		in {
+			devShells.default = pkgs.mkShell {
+				buildInputs = with pkgs; [
+					# Core
+					pacman
+					arch-install-scripts
+					git
+					parted
+
+					(python314.withPackages (ps: [ ps.pyparted ]))
+
+					# Filesystem tools
+					dosfstools
+					e2fsprogs
+					btrfs-progs
+					xfsprogs
+					f2fs-tools
+					cryptsetup
+					lvm2
+
+					# Utils
+					coreutils
+					util-linux
+					pciutils
+					kbd
+					libxcrypt
+					tzdata
+				];
+
+				# libxcrypt is dlopen'd by crypt.py via ctypes; buildInputs only
+				# wires link-time, so expose it on the runtime linker path for
+				# the .so lookup.
+				shellHook = ''
+					export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [ pkgs.libxcrypt ]}:$LD_LIBRARY_PATH
+				'';
+			};
+		}
+	);
+}
