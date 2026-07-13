587c05d7fff8565590c428307b159060428eb524
h8d13 <hadean-eon-dev@proton.me>
2026-07-13 12:40:40 +0200

    misc(ver)
    Signed-off-by: h8d13 <hadean-eon-dev@proton.me>

--- archinstoo/archinstoo/_version.py
+++ archinstoo/archinstoo/_version.py
@@ -1,2 +1,9 @@
+import subprocess
+
+def git_shash() -> str:
+        return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
+def git_branch() -> str:
+         return subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).decode('ascii').strip()
+
 __license__ = '- Copyright (C) 2026 - Hadean Eon'
-__version__ = f'0.1.13-4 {__license__}'
+__version__ = f'0.1.13-4 ({git_branch()}-{git_shash()}) {__license__}'
