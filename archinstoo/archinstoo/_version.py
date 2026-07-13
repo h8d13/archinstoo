44b2f76e8c9deab7ab951a8845900b7172d5d9af
h8d13 <hadean-eon-dev@proton.me>
2026-07-13 12:50:35 +0200

    fix(ci)
    Signed-off-by: h8d13 <hadean-eon-dev@proton.me>

--- archinstoo/archinstoo/_version.py
+++ archinstoo/archinstoo/_version.py
@@ -1,9 +1,13 @@
 import subprocess
 
-def git_shash() -> str:
-        return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
-def git_branch() -> str:
-         return subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).decode('ascii').strip()
 
+def git_shash() -> str:
+	return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()  # noqa: S607 - git from $PATH
+
+
+def git_branch() -> str:
+	return subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).decode('ascii').strip()  # noqa: S607 - git from $PATH
+
+__gitstat__ = f'{git_branch()}-{git_shash()}'
 __license__ = '- Copyright (C) 2026 - Hadean Eon'
-__version__ = f'0.1.13-4 ({git_branch()}-{git_shash()}) {__license__}'
+__version__ = f'0.1.13-4 ({__gitstat__}) {__license__}'
