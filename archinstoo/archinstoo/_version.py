6f5f4506f4f9320e1c3548c65f3ead9100ae9e76
h8d13 <hadean-eon-dev@proton.me>
2026-07-13 14:46:42 +0200

    misc(ver)
    Signed-off-by: h8d13 <hadean-eon-dev@proton.me>

--- archinstoo/archinstoo/_version.py
+++ archinstoo/archinstoo/_version.py
@@ -2,13 +2,18 @@ import subprocess
 
 
 def git_shash() -> str:
-	return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()  # noqa: S607 - git from $PATH
+	return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'], stderr=subprocess.DEVNULL).decode('ascii').strip()  # noqa: S607 - git from $PATH
 
 
 def git_branch() -> str:
-	return subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).decode('ascii').strip()  # noqa: S607 - git from $PATH
+	return subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], stderr=subprocess.DEVNULL).decode('ascii').strip()  # noqa: S607 - git from $PATH
 
 
-__gitstat__ = f'{git_branch()}-{git_shash()}'
+# installed package: no git binary (OSError) or not a repo (CalledProcessError)
+try:
+	__gitstat__ = f'{git_branch()}-{git_shash()}'
+except OSError, subprocess.CalledProcessError:
+	__gitstat__ = 'DEV'
+
 __license__ = '- Copyright (C) 2026 - Hadean Eon'
 __version__ = f'0.1.13-4 ({__gitstat__}) {__license__}'
